ab2c406f09fec6b3c8940f27783bf1393b2244b4
Sagar Gaur <sagar.gaur@mindtickle.com>
2018-06-18 13:17:55 +0530

    #6. Changing states to X and O etc.

--- src/index.js
+++ src/index.js
@@ -11,47 +11,52 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-constructor(props) {
-    super(props);
-    this.state = {
-        squares: Array(9).fill(null),
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: false,
+        }
     }
-}
 
-handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
-}
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext?'X':'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
 
-renderSquare(i) {
-    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
-}
+    renderSquare(i) {
+        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+    }
 
-render() {
-    const status = 'Next player: X';
+    createBoard = () => {
+        let board = []
 
-    return (
-    <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-        {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
+        // Outer loop to create parent
+        for (let i = 0; i < 3; i++) {
+            let children = []
+            for (let j = 0; j < 3; j++) {
+                children.push(this.renderSquare(3*i+j))
+            }
+            //Create the parent and add the children
+            board.push(<div className="board-row">{children}</div>)
+        }
+        return board
+    }
+
+    render() {
+        const status = 'Next player: ' + (this.state.xIsNext?'X':'O');
+
+        return (
+        <div>
+            <div className="status">{status}</div>
+            {this.createBoard()}
         </div>
-        <div className="board-row">
-        {this.renderSquare(3)}
-        {this.renderSquare(4)}
-        {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-        {this.renderSquare(6)}
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
-        </div>
-    </div>
-    );
-}
+        );
+    }
 }
 
 class Game extends React.Component {
